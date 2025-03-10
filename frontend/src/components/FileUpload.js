2d033e458480be21a8e9b04f0eb1d80f12129f6f
angel <angel20170238b@gmail.com>
2025-03-10 09:00:55 -0500

    commit

---
+++ frontend/src/components/FileUpload.js
@@ -0,0 +1,10 @@
+function FileUpload() {
+    return (
+        <div>
+            <input/>
+            <button >Upload and Predict</button>
+        </div>
+    );
+};
+
+export default FileUpload;
